2b2b2c3d7abaea3813a2968fd73b6f30897bf6b3
agaralambinia <alexandariagara@gmail.com>
2023-11-15 18:45:25 +0400

    finished mandatory part

--- curriculum/lib_ft/libft.h
+++ curriculum/lib_ft/libft.h
@@ -2,6 +2,12 @@
 # define LIBFT_H
 
 #include <stdlib.h>
+#include <unistd.h>
+typedef struct s_list
+{
+void *content;
+struct s_list *next;
+} t_list;
 int	    ft_isalpha(int str);
 int	    ft_isdigit(int str);
 int	    ft_isalnum(int str);
@@ -32,5 +38,9 @@ char    **ft_split(char const *s, char c);
 char    *ft_itoa(int n);
 char    *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void    ft_striteri(char *s, void (*f)(unsigned int, char*));
+void    ft_putchar_fd(char c, int fd);
+void    ft_putstr_fd(char *s, int fd);
+void    ft_putendl_fd(char *s, int fd);
+void    ft_putnbr_fd(int n, int fd);
 
 #endif
